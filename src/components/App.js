02527b77d54ca0393f63049e55fdcdacec713169
Megha <megha13dandapat@gmail.com>
2021-08-02 22:13:45 +0530

    signup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
+import Signup from './Signup'
+import {Container} from 'react-bootstrap'
 function App() {
   return (
-    'Hello world'
+    <Container className="d-flex align-items-center justify-content-center" 
+    style={{minHeight:"100vh"}}>
+    <div className="w-100" style={{maxWidth: "400px"}}>
+   <Signup/>
+   </div>
+   </Container>
   );
 }
 
