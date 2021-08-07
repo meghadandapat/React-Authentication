c8a58f0c65b48513ab1c43ffbb631388926166dd
Megha <megha13dandapat@gmail.com>
2021-08-07 13:04:28 +0530

    login using firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import Signup from './Signup'
-import {Container} from 'react-bootstrap'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" 
-    style={{minHeight:"100vh"}}>
-    <div className="w-100" style={{maxWidth: "400px"}}>
-   <Signup/>
-   </div>
-   </Container>
+    <Container fluid className="p-0">
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </Container>
   );
 }
 
---
+++ src/components/Dashboard.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Nav, Button, Navbar, Container } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { useHistory } from "react-router-dom";
+
+export default function Dashboard() {
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
+  return (
+    <>
+      <Navbar expand="md"  bg="dark" variant="dark">
+        <Container>
+         
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+              <Nav.Link href="/">Dashboard</Nav.Link>
+              </Nav>
+            <Nav>
+              <Button variant="light" onClick={handleLogout}>
+                Log Out
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
